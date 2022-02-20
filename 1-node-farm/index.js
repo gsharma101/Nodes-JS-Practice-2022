de5726c32efe6bf887ac02ab18021f3b82e9ad36
gsharma101 <sharmag8121@gmail.com>
2022-02-20 23:33:03 +0530

    Working with nodes

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,8 @@
-'using strict';
+
 const fs = require('fs');
 /*
 const hello = 'Hello World';
 console.log(hello);
 */
-fs.readFileSync();
+const textIn = fs.readFileSync('txt/input.txt', 'utf-8');
+console.log(textIn);
