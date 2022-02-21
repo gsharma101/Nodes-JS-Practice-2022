22771568716241a831386e40eda78529bf8f3bd8
gsharma101 <sharmag8121@gmail.com>
2022-02-21 22:15:47 +0530

    completed reading and writting

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,10 @@
-
-const fs = require('fs');
+const fs = require("fs");
 /*
 const hello = 'Hello World';
 console.log(hello);
 */
-const textIn = fs.readFileSync('txt/input.txt', 'utf-8');
+const textIn = fs.readFileSync("txt/input.txt", "utf-8");
 console.log(textIn);
+const textOut = `This is what we know about the avocado: ${textIn}\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written');
